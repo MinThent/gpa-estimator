ed2617badf2a437ddee50807bd849f24921efa1a
airfrag <80678006+MinThent@users.noreply.github.com>
2024-08-18 16:17:12 +0900

    small width changes

--- src/components/GradeInput.jsx
+++ src/components/GradeInput.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function GradeInput({ text, value, setValue }) {
   return (
-    <div className="flex flex-col items-start w-2/5 sm:w-2/5">
+    <div className="flex flex-col items-start w-4/5 sm:w-2/5 md:w-2/5 lg:w-2/5">
       <label
         htmlFor="numberInput"
         className="mb-1 font-medium text-slate-100 text-sm"
