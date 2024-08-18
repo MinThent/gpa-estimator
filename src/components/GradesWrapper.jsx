b1808f1ad8fa36c4ffe00339574124e052eb3e46
airfrag <80678006+MinThent@users.noreply.github.com>
2024-08-18 16:30:56 +0900

    another width changes for responsiveness

--- src/components/GradesWrapper.jsx
+++ src/components/GradesWrapper.jsx
@@ -33,7 +33,7 @@ export default function GradesWrapper({
       <button
         type="button"
         onClick={calculateEstimation}
-        className="bg-blue-500 hover:bg-blue-600 mt-4 px-10 py-2 rounded w-2/5 sm:w-2/5 text-white"
+        className="bg-blue-500 hover:bg-blue-600 mt-4 px-10 py-2 rounded w-2/5 sm:w-1/3 md:w-1/5 lg:w-1/5 text-white"
       >
         Estimate
       </button>
